1192186de8c8413c64a8578286d181194036fe56
Péterson Fagundes <petersonwrf@gmail.com>
2020-04-27 19:37:41 -0300

    Adicionando arquivos e ajustando POM para uso do migration

---
+++ 07 RestWithSpringBootUdemy Adicionando Migrations/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Itajubá - Brasil  - MG', 'Péterson', 'Male', 'Fagundes'),
+	(2, 'PA - Brasil  - MG', 'Ursula', 'Female', 'Fagundes'),
+	(3, 'Itajubá - Brasil  - MG', 'Woodson', 'Male', 'Fagundes'),
+	(8, 'Itajubá - Brasil  - MG', 'BaaSon', 'Male', 'Fagundes');
